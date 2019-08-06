f56034a2811e48a606a61b81467ba0b7dc84b894
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2019-08-06 11:46:54 -0500

    docs: ejemplos de select

--- databases/msyql/dml.sql
+++ databases/msyql/dml.sql
@@ -71,6 +71,78 @@ truncate table ceet_surrogate.cliente;
 
 insert into ceet_surrogate.tipo_documento(sigla, nombre_documento, estado) VALUE ('RT','rastro','Activo');
 
+-- select
+select * from observador_de_proyectos.cliente;
+select tipo_documento,numero_documento from observador_de_proyectos.cliente;
+
+select null;
+select 1+2;
+select 'hola';
+select now();
+
+SELECT * FROM observador_de_proyectos.ficha
+where numero_ficha!='1803182G2'; -- en el where se puede usar los operadores relacionales > < = != >= <=
+select * from observador_de_proyectos.cliente
+where primer_nombre='hernando';
+select count(*) from observador_de_proyectos.cliente_has_rol
+where fecha_terminacion>'2019-08-06'
+;
+
+select * from observador_de_proyectos.cliente where segundo_apellido is null;
+select * from observador_de_proyectos.cliente where segundo_apellido is not null;
+
+
+-- between
+
+select count(*) from observador_de_proyectos.cliente_has_rol
+where   fecha_terminacion>='2019-01-01' and fecha_terminacion<='2019-12-31'
+;
+select count(*) from observador_de_proyectos.cliente_has_rol
+where fecha_terminacion between  '2019-01-01' and '2019-12-31'
+;
+
+-- like
+
+select * from observador_de_proyectos.cliente where primer_nombre like '%z%';
+
+select * from observador_de_proyectos.cliente where primer_nombre like '___W%';
+
+-- in
+select * from observador_de_proyectos.cliente_has_rol where rol = 'Líder de área' or rol = 'Instructor' or rol= 'Aprendiz';
+
+select *
+from observador_de_proyectos.cliente_has_rol where rol in ('Líder de área', 'Insructor', 'aprendiz');
+
+-- when
+select tipo_documento,numero_documento, case rol when 'Aprendiz' then 'estudiante' else 'profesor'  end from observador_de_proyectos.cliente_has_rol
+;
+
+-- is
+select numero_documento, tipo_documento >= 'TI' is true as 'menor de edad' from observador_de_proyectos.cliente
+where numero_documento='1001174847'
+;
+
+-- binary
+select * from observador_de_proyectos.cliente where binary (primer_nombre) Like 'JOHN';
+
+-- regex
+select * from observador_de_proyectos.usuario where !(correo regexp '^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$') ;
+
+--
+
+create table ddl.ejemplo(
+    id int primary key auto_increment,
+    campo1 int,
+    campo2 float,
+    campo3 varchar(100)
+);
+
+
+
+
+
+
+
 
 
 
